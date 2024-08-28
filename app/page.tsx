f8ab5f1fef47b5b41f85ba6224995db1726186ba
mbendev <contact@mbendev.com>
2024-08-28 15:54:07 +0200

    update color

--- app/page.tsx
+++ app/page.tsx
@@ -3,49 +3,51 @@ import React from "react";
 import Particles from "./components/particles";
 
 const navigation = [
-	// { name: "Projects", href: "/projects" },
-	{ name: "Contact", href: "/contact" },
+  { name: "Projects", href: "/projects", disabled: true },
+  { name: "Contact", href: "/contact", disabled: false },
 ];
 
 export default function Home() {
-	return (
-		<div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
-			<nav className="my-16 animate-fade-in">
-				<ul className="flex items-center justify-center gap-4">
-					{navigation.map((item) => (
-						<Link
-							key={item.href}
-							href={item.href}
-							className="text-sm duration-500 text-zinc-500 hover:text-zinc-300"
-						>
-							{item.name}
-						</Link>
-					))}
-				</ul>
-			</nav>
-			<div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-			<Particles
-				className="absolute inset-0 -z-10 animate-fade-in"
-				quantity={100}
-			/>
-			<h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-				mbendev
-			</h1>
+  return (
+    <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
+      <nav className="my-16 animate-fade-in">
+        <ul className="flex items-center justify-center gap-4">
+          {navigation.map((item) => (
+            <Link
+              key={item.href}
+              href={item.disabled ? "" : item.href}
+              className={`text-sm duration-500 text-zinc-500 hover:text-zinc-300 ${
+                item.disabled ? "cursor-not-allowed" : ""
+              }`}
+            >
+              {item.name}
+            </Link>
+          ))}
+        </ul>
+      </nav>
+      <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+      <Particles
+        className="absolute inset-0 -z-10 animate-fade-in"
+        quantity={100}
+      />
+      <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-red-mbendev cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
+        mbendev
+      </h1>
 
-			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-			<div className="my-16 text-center animate-fade-in">
-				<h2 className="text-sm text-zinc-500 ">
-					Une idée ?{" "}
-					<Link
-						target="_blank"
-						href="https://unkey.dev"
-						className="underline duration-500 hover:text-zinc-300"
-					>
-						contactez moi
-					</Link>{" "}
-					pour transformer votre idée en produit.
-				</h2>
-			</div>
-		</div>
-	);
+      <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+      <div className="my-16 text-center animate-fade-in">
+        <h2 className="text-sm text-zinc-500 ">
+          Une idée ?{" "}
+          <Link
+            target="_blank"
+            href="/contact"
+            className="underline duration-500 hover:text-zinc-300"
+          >
+            Contactez moi
+          </Link>{" "}
+          pour transformer votre idée en produit.
+        </h2>
+      </div>
+    </div>
+  );
 }
