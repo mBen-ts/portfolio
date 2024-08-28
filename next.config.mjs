fe8fc2d2f93c6a5cd46f7280bf944878d6bdef6c
mbendev <contact@mbendev.com>
2024-08-28 16:04:52 +0200

    debug shiki & remove yarn.lock

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,11 @@ import { withContentlayer } from "next-contentlayer";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-	experimental: {
-		mdxRs: true,
-	},
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+  experimental: {
+    mdxRs: true,
+    serverComponentsExternalPackages: ["shiki"],
+  },
 };
 
 export default withContentlayer(nextConfig);
